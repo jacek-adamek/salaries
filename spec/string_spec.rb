184bea2ce79a0aead7664fb0bcc1c452188a1cbf
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-22 12:04:28 +0200

    Fixing typo

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -2,7 +2,7 @@ require_relative "../salaries"
 
 describe "String" do
   describe "#to_ascii" do
-    it "transcode to ascii" do
+    it "transcodes to ascii" do
       expect("ąćęłóńśżź".to_ascii).to eq "acelonszz"
     end
   end
