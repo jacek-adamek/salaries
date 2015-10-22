6a6d3e0ebe4fdd4033cfcfe02e1d390a1645aec5
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-22 15:16:26 +0200

    Adding upper case letters to to_ascii conversion

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -3,7 +3,7 @@ require_relative "../salaries"
 describe "String" do
   describe "#to_ascii" do
     it "transcodes to ascii" do
-      expect("ąćęłóńśżź".to_ascii).to eq "acelonszz"
+      expect("ąćęłóńśżźĄĆĘŁÓŃŚŻŹ".to_ascii).to eq "acelonszzACELONSZZ"
     end
   end
 end
